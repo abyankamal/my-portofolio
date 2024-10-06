4ed8aec81fff5d059b44f3ef2c92f1d8e60d6804
abyankamal <abyankamal8@gmail.com>
2024-10-06 11:38:26 +0700

    15%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import Typewriter from "./components/Typewriter";
 import UK from "@/app/public/english.png";
 import Laravel from "@/app/public/laravel.png";
-import MongoDB from "@/app/public/mongodb.png";
-import Python from "@/app/public/python.jpg";
+import Python from "@/app/public/python.png";
+import NextJS from "@/app/public/nextjs.png";
+import Tailwind from "@/app/public/tailwind.png";
+import Vue from "@/app/public/vue.png";
 
 export default function Home() {
   return (
@@ -23,20 +25,20 @@ export default function Home() {
         About Me
       </h1>
       <p className="text-wrap text-white mx-auto p-5 text-justify mb-5">
-        Hello, my name is Muhammad Abyan Kamal. I am a graduate from Garut
-        Institute of Technology who has a passion for technology, especially
+        Hello, my name is Muhammad Abyan Kamal. I am a graduate from Institut
+        Teknologi Garut who has a passion for technology, especially
         programming.
       </p>
       {/* skills */}
       <h1 className="text-2xl font-semibold text-white text-center pt-8">
-        My Skills
+        Skills
       </h1>
       <div className="grid grid-cols-5 gap-4 mx-auto p-5">
         <Image src={Laravel} alt="uk" />
         <Image src={Python} alt="uk" />
-        <Image src={UK} alt="uk" />
-        <Image src={UK} alt="uk" />
-        <Image src={UK} alt="uk" />
+        <Image src={NextJS} alt="uk" />
+        <Image src={Vue} alt="uk" />
+        <Image src={Tailwind} alt="uk" />
       </div>
       <ul>
         <li>HTML</li>
@@ -54,7 +56,9 @@ export default function Home() {
         <li>Laravel</li>
       </ul>
       {/* projects */}
-      <p>Here are some of my projects</p>
+      <p className="text-2xl font-semibold text-white text-center pt-8">
+        Here are some of my projects
+      </p>
       {/* contact */}
       <p>You can contact me at Below</p>
       {/* footer */}
