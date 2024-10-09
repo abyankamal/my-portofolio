eaec75999d4255375daa8995d18de6b386000675
abyankamal <abyankamal8@gmail.com>
2024-10-09 11:31:49 +0700

    20%

--- src/app/components/ProjectList.tsx
+++ src/app/components/ProjectList.tsx
@@ -1,22 +1,71 @@
+import Image from "next/image"; // Correctly import the Image component
+import bwajobhunt from "@/app/public/bwajobhunt.png";
+import thewildoasis from "@/app/public/thewildoasis.png";
+
+import { StaticImageData } from "next/image";
+
 export default function ProjectList() {
+  type ProjectList = {
+    name: string;
+    description: string;
+    image: StaticImageData;
+    techStack: string[];
+  };
+
+  const projectList: ProjectList[] = [
+    {
+      name: "JobHuntly",
+      description: "This is a Job Hunting Website when you can find some jobs",
+      image: bwajobhunt,
+      techStack: ["React", "NextJS", "TailwindCSS", "Prisma", "PostgreSQL"],
+    },
+    {
+      name: "JobHuntly",
+      description: "This is a website for booking hotel with many features",
+      image: thewildoasis,
+      techStack: [
+        "React",
+        "NextJS",
+        "TailwindCSS",
+        "Prisma",
+        "PostgreSQL",
+        "Supabase",
+        "NextAuth",
+      ],
+    },
+  ];
+
   return (
-    <div className="grid grid-flow-col gap-4 mx-auto p-5">
-      <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-        <div className="px-4 py-5 sm:px-6">
-          {/* Content goes here */}
-          <p className="text-2xl font-semibold text-white text-center pt-8"></p>
-          {/* We use less vertical padding on card headers on desktop than on body sections */}
+    <div className="grid grid-flow-row gap-4 mx-auto p-5">
+      {projectList.map((project, index) => (
+        <div
+          key={index} // Add a unique key for each element in the list
+          className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow"
+        >
+          <div className="px-4 py-5 sm:px-6">
+            {/* Header Content */}
+            <p className="text-2xl font-semibold text-center">{project.name}</p>
+          </div>
+          <div className="px-4 py-5 sm:p-6">
+            {/* Image Content */}
+            <Image
+              src={project.image}
+              alt={project.name} // Use the project name as alt text// Specify dimensions explicitly
+              className="mx-auto"
+            />
+          </div>
+          <div className="px-4 py-5 sm:p-6">
+            {/* Image Content */}
+            <p className="text-md font-normal text-center">
+              {project.description}
+            </p>
+          </div>
+          <div className="px-4 py-4 sm:px-6">
+            {/* Footer Content */}
+            <p>Tech Stack: {project.techStack.join(", ")}</p>
+          </div>
         </div>
-        <div className="px-4 py-5 sm:p-6">
-          {/* Content goes here */}
-          <Image src={NextJS} alt="uk" width={100} className="mx-auto" />
-        </div>
-        <div className="px-4 py-4 sm:px-6">
-          {/* Content goes here */}
-          Tech Stack : React, NextJS, TailwindCSS
-          {/* We use less vertical padding on card footers at all sizes than on headers or body sections */}
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Python from "@/app/public/python.png";
 import NextJS from "@/app/public/nextjs.png";
 import Tailwind from "@/app/public/tailwind.png";
 import Vue from "@/app/public/vue.png";
+import ProjectList from "./components/ProjectList";
 
 type NavigationItem = {
   name: string;
@@ -94,24 +95,7 @@ export default function Home() {
         <p className="text-2xl font-semibold text-white text-center pt-8">
           Here are some of my projects
         </p>
-        <div className="grid grid-flow-col gap-4 mx-auto p-5">
-          <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-            <div className="px-4 py-5 sm:px-6">
-              {/* Content goes here */}
-              <p>Judul Project</p>
-              {/* We use less vertical padding on card headers on desktop than on body sections */}
-            </div>
-            <div className="px-4 py-5 sm:p-6">
-              {/* Content goes here */}
-              <Image src={NextJS} alt="uk" width={100} className="mx-auto" />
-            </div>
-            <div className="px-4 py-4 sm:px-6">
-              {/* Content goes here */}
-              Tech Stack : React, NextJS, TailwindCSS
-              {/* We use less vertical padding on card footers at all sizes than on headers or body sections */}
-            </div>
-          </div>
-        </div>
+        <ProjectList />
         {/* contact */}
         <footer className="bg-white">
           <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
