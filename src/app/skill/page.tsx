ffaa42a41070255d29e19e6f2832a57120f98a69
abyankamal <abyankamal8@gmail.com>
2025-01-10 17:47:45 +0700

    add canvas

--- src/app/skill/page.tsx
+++ src/app/skill/page.tsx
@@ -3,7 +3,7 @@ import Laravel from "@/app/public/laravel.png";
 import Python from "@/app/public/python.png";
 import NextJS from "@/app/public/nextjs.png";
 import Tailwind from "@/app/public/tailwind.png";
-import Vue from "@/app/public/vue.png";
+// import Vue from "@/app/public/vue.png";
 import Bootstrap from "@/app/public/bootstrap.png";
 import Adonis from "@/app/public/adonis.png";
 import Canvas from "../components/Canvas";
@@ -15,7 +15,7 @@ export default function Skill() {
         <SkillCard src={Laravel} alt="Laravel" />
         <SkillCard src={Python} alt="Python" />
         <SkillCard src={NextJS} alt="Next.js" />
-        <SkillCard src={Vue} alt="Vue.js" />
+        {/* <SkillCard src={Vue} alt="Vue.js" /> */}
         <SkillCard src={Tailwind} alt="Tailwind CSS" />
         <SkillCard src={Bootstrap} alt="Bootstrap" />
         <SkillCard src={Adonis} alt="Adonis" />
